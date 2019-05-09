5757a21268fd23508a691cfdc658a0b6f13d420d
João Alves <joaoalves.cs@gmail.com>
2019-05-09 17:39:59 -0300

    Criação da página enviar.php e alteração nas outras páginas.

---
+++ enviar.php
@@ -0,0 +1,18 @@
+<?
+    $destinatario = "joaoalves.cs@gmail.com";
+
+    $nome = $_REQUEST['nome'];
+    $email = $_REQUEST['email'];
+    $comentario = $_REQUEST['comentario'];
+
+    // monta o e-mail na variável $body
+
+    $body = " ======= Olá, recebemos um comentário! =======\n";
+    $body = $body . "Nome: " . $nome . "\n";
+    $body = $body . "E-mail" . $email . "\n";
+    $body = $body . "Comentário" . $comentario . "\n";
+    $body = " ============================================= \n ";
+
+    // envia o email
+    mail($destinatario, $comentario , $body, "From: $email\r\n");
+?>
