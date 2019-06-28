436bdca26cf22fd1d2dbee70b404e17d5bd4b663
João Alves <joaoalves.cs@gmail.com>
2019-06-27 21:58:00 -0300

    Atividade feita

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ function Enviar() {
     var nome = document.getElementById("nomeid");
  
     if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+        alert('Sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso! Entraremos em contato em breve!');
+    }
+ 
+}
+
+function Enviar1() {
+ 
+    var nome = document.getElementById("nomeid");
+ 
+    if (nome.value != "") {
+        alert('Sr(a) ' + nome.value + ' seu comentário nos ajudará a melhorar nosso site. Obrigado!');
     }
  
 }
